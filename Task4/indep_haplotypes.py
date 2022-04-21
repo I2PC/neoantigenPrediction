d6ef3279d158d07c18a12a76d53c28dc600edd35
ivan-ea <ivan.ea7h@gmail.com>
2022-04-21 23:14:51 +0200

    Separate script for downloading epitopes from differnt haplotypes independently

--- Task4/indep_haplotypes.py
+++ Task4/indep_haplotypes.py
@@ -3,14 +3,22 @@
 import pandas as pd
 import numpy as np
 import time
-
+import sys
 verb = True # to print debug info
 
+# 0. Select the haplotype
+haplotypes = ['MHC_1_A','MHC_1_B','MHC_1_C','MHC_2_DP','MHC_2_DQ','MHC_2_DR']
+
+h_index = 0 if len(sys.argv) == 1 else int(sys.argv[1]) 
+if h_index > 5 or h_index < 0:
+  print('ERROR: The argument must be a number between 0 and 5')
+  sys.exit(1)
+
+if verb: print('Generating epitope/non-epitope data for haplotype {}: {}'.
+  format(h_index,haplotypes[h_index]),end='\n\n')
 
 # 1. Load the data
 # There is one epitope file per haplotype
-
-haplotypes = ['MHC_1_A','MHC_1_B','MHC_1_C','MHC_2_DP','MHC_2_DQ','MHC_2_DR']
 
 # Public repository with all the data: https://gitlab.com/ivan_ea/epitopes
 DATA_URL = 'https://gitlab.com/ivan_ea/epitopes/-/raw/master/'
@@ -26,21 +34,20 @@ if verb:
   print('\n Explanation of the columns of the epitope files:')
   print(pd.read_csv(DATA_URL+'explain_columns.csv'), end='\n\n')
 
-epitopes_dfs = {}
-
-for h in haplotypes: # Takes like 5 seconds 
-  if verb: print('Fetching {}.csv...'.format(h), end=' ')
-  epitopes_dfs[h] = pd.read_csv(DATA_URL+h+'.csv')
-  # remove epitopes without start and end information (only 252 out of 600k)
-  epitopes_dfs[h].dropna(subset=['start'], inplace = True) 
-  # now columns 'start' and 'end' can be treated as ints 
-  epitopes_dfs[h] = epitopes_dfs[h].astype({'start': int, 'end': int})
-
-  if verb: print('It has {} epitopes'.format(len(epitopes_dfs[h])))
+h = haplotypes[h_index] 
+if verb: print('Fetching {}.csv...'.format(h), end=' ')
+epitopes_df = pd.read_csv(DATA_URL+h+'.csv')
+# remove epitopes without start and end information (only 250 out of 600k)
+epitopes_df.dropna(subset=['start'], inplace = True) 
+# now columns 'start' and 'end' can be treated as ints 
+epitopes_df = epitopes_df.astype({'start': int, 'end': int})
+if verb: print('It has {} epitopes'.format(len(epitopes_df)))
 
 if verb:
   print('\n Beginning of the {} epitopes dataframe:'.format(h))
-  print(epitopes_dfs[h].head()) 
+  print(epitopes_df.head()) 
+
+sys.exit(0)
 
 # 2. Once data is loaded we want to create a new dataframe containing the epitope, the aa chain and the protein id
 # TAKES A LOT OF TIME 3,5 H 
@@ -49,9 +56,7 @@ start_time = time.time()
 
 protein_epitope_df = pd.DataFrame()
 
-if verb: print('\n Finding epitopes for protein n: ',end='')
-for index, protein in proteins_df.iterrows():
-  #if verb:  print(index, end=', ') #debug
+for index,protein in proteins_df.iterrows():
   protein_id = protein['protein_id']
   aa_chain = protein['aas']
   #Once we get the protein_id we search for that protein in the haplotypes dfs, and create a new dataframe with the 
